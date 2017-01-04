0d7785e7806f083e8e99b94370515733123b7a4f
shenqiang <13032119695@163.com>
2017-01-04 17:23:47 +0800

    2017、1、4 today i am busy!

--- js/common/bdapp.js
+++ js/common/bdapp.js
@@ -149,3 +149,18 @@ function getLocalTime(nS) {
     return new Date(parseInt(nS) * 1000).toLocaleString().substr(0,17)
 }
  
+
+//当前时间转时间戳
+function datetime_to_unix(datetime){
+    var tmp_datetime = datetime.replace(/:/g,'-');
+    tmp_datetime = tmp_datetime.replace(/ /g,'-');
+    var arr = tmp_datetime.split("-");
+    var now = new Date(Date.UTC(arr[0],arr[1]-1,arr[2],arr[3]-8,arr[4],arr[5]));
+    return parseInt(now.getTime()/1000);
+}
+ 
+// 时间戳转时间
+function unix_to_datetime(unix) {
+    var now = new Date(parseInt(unix) * 1000);
+    return now.toLocaleString().replace(/年|月/g, "-").replace(/日/g, " ");
+}
