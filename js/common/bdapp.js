c7669066fa3e386eabad1cc96e8de999a48986f9
shenqiang <13032119695@163.com>
2017-01-05 13:42:53 +0800

    2017/1/5我简直就是天才

--- js/common/bdapp.js
+++ js/common/bdapp.js
@@ -164,3 +164,30 @@ function unix_to_datetime(unix) {
     var now = new Date(parseInt(unix) * 1000);
     return now.toLocaleString().replace(/年|月/g, "-").replace(/日/g, " ");
 }
+
+
+ //js截取字符串，中英文都能用
+//如果给定的字符串大于指定长度，截取指定长度返回，否者返回源字符串。
+function cutstr(str, len) {
+    var str_length = 0;
+    var str_len = 0;
+    str_cut = new String();
+    str_len = str.length;
+    for (var i = 0; i < str_len; i++) {
+        a = str.charAt(i);
+        str_length++;
+        if (escape(a).length > 4) {
+            //中文字符的长度经编码之后大于4
+            str_length++;
+        }
+        str_cut = str_cut.concat(a);
+        if (str_length >= len) {
+            str_cut = str_cut.concat("...");
+            return str_cut;
+        }
+    }
+    //如果给定字符串小于指定长度，则返回源字符串；
+    if (str_length < len) {
+        return str;
+    }
+}
